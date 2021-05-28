13ae2dd3b9758f124ab30d2ec3b9e84296769f64
Anonymous <git-anon@protonmail.com>
2021-05-28 14:10:59 -0700

    Update FAQ and build

--- src/pages/Faq.vue
+++ src/pages/Faq.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="row justify-content-center">
         <div class="col-12 col-md-10 col-lg-8 col-xl-10">
-            <expandable-card v-for="[question, answer] of faqs">
-                <template #title><span v-html="question" /></template>
+            <expandable-card v-for="([question, answer], idx) of faqs">
+                <template #title>#{{ idx + 1}}.&nbsp; &nbsp;<span v-html="question" /></template>
                 <template #content><div v-html="answer" /></template>
             </expandable-card>
         </div>
